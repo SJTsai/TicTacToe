29b9ebc57610dbf6d212857bbe4d8b308ff6c433
Simon Tsai <simon.tsai.j@gmail.com>
2014-12-27 17:21:31 -0500

    Added required TicTacToeBoardEntity dependency to DefaultWinnerVerifier.

--- TicTacToe/src/main/domain/verification/DefaultWinnerVerifier.java
+++ TicTacToe/src/main/domain/verification/DefaultWinnerVerifier.java
@@ -5,6 +5,12 @@ import main.domain.entities.TicTacToeBoardEntity;
 import main.domain.verification.interfaces.WinnerVerifier;
 
 public class DefaultWinnerVerifier implements WinnerVerifier {
+  
+  private TicTacToeBoardEntity board;
+  
+  public DefaultWinnerVerifier(TicTacToeBoardEntity board) {
+    this.board = board;
+  }
 
   @Override
   public boolean verifyWinForBoardAndLastMoveMade(TicTacToeBoardEntity board, MoveEntity lastMoveMade) {
