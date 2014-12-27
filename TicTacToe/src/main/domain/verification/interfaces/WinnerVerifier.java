6cca86502b08dc624aef14cdf1c9b99cdb931a42
Simon Tsai <simon.tsai.j@gmail.com>
2014-12-27 16:16:23 -0500

    Added WinnerVerifier interface.

---
+++ TicTacToe/src/main/domain/verification/interfaces/WinnerVerifier.java
@@ -0,0 +1,9 @@
+package main.domain.verification.interfaces;
+
+import main.domain.entities.MoveEntity;
+import main.domain.entities.TicTacToeBoardEntity;
+
+public interface WinnerVerifier {
+
+  public boolean verifyWinForBoardAndLastMoveMade(TicTacToeBoardEntity board, MoveEntity lastMoveMade);
+}
